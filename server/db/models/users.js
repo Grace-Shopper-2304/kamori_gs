b2fee3377001881925ccc43a06eebd9de14c7449
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-07-19 09:30:15 -0400

    created db model for Users

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -2,13 +2,52 @@ const crypto = require('crypto')
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const User = db.define('user', {
+const Users = db.define('users', {
   email: {
     type: Sequelize.STRING,
+    allowNull: false,
     unique: true,
+    validate: {
+      isEmail: true,
+      notEmpty: true
+    }
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  address: {
+    type: Sequelize.TEXT,
     allowNull: false
   },
+  phone: {
+    type: Sequelize.STRING,
+    validate: {
+      isPhoneNumber(value) {
+        const phoneRegex = /^\+1\s[0-9]{3}-[0-9]{3}-[0-9]{4}$/ // assuming all orders are from the US
+        // we should add some autoformatting when a user inputs their phone so that it matches +1 xxx-xxx-xxxx
+        if (!phoneRegex.test(value)) {
+          throw new Error('Invalid phone number')
+        }
+      }
+    }
+  },
+  admin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
   password: {
+    //pw & salt are boilerplate, leaving them in case we need them later
     type: Sequelize.STRING,
     // Making `.password` act like a func hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
@@ -23,29 +62,28 @@ const User = db.define('user', {
     get() {
       return () => this.getDataValue('salt')
     }
-  },
-  googleId: {
-    type: Sequelize.STRING
   }
 })
 
-module.exports = User
+module.exports = Users
+
+// below is all boilerplate
 
 /**
  * instanceMethods
  */
-User.prototype.correctPassword = function(candidatePwd) {
-  return User.encryptPassword(candidatePwd, this.salt()) === this.password()
+Users.prototype.correctPassword = function(candidatePwd) {
+  return Users.encryptPassword(candidatePwd, this.salt()) === this.password()
 }
 
 /**
  * classMethods
  */
-User.generateSalt = function() {
+Users.generateSalt = function() {
   return crypto.randomBytes(16).toString('base64')
 }
 
-User.encryptPassword = function(plainText, salt) {
+Users.encryptPassword = function(plainText, salt) {
   return crypto
     .createHash('RSA-SHA256')
     .update(plainText)
@@ -58,13 +96,13 @@ User.encryptPassword = function(plainText, salt) {
  */
 const setSaltAndPassword = user => {
   if (user.changed('password')) {
-    user.salt = User.generateSalt()
-    user.password = User.encryptPassword(user.password(), user.salt())
+    user.salt = Users.generateSalt()
+    user.password = Users.encryptPassword(user.password(), user.salt())
   }
 }
 
-User.beforeCreate(setSaltAndPassword)
-User.beforeUpdate(setSaltAndPassword)
-User.beforeBulkCreate(users => {
+Users.beforeCreate(setSaltAndPassword)
+Users.beforeUpdate(setSaltAndPassword)
+Users.beforeBulkCreate(users => {
   users.forEach(setSaltAndPassword)
 })
