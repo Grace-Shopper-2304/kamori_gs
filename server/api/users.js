20db25ffbd0981f1ff69b0f7b996f5a2d5e95e9e
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-07-20 10:15:44 -0400

    fixed /api/users route

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,17 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {Users} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
+    const users = await Users.findAll()
     res.json(users)
   } catch (err) {
     next(err)
   }
 })
+
+// explicitly select only the id and email fields - even though
+// users' passwords are encrypted, it won't help if we just
+// send everything to anyone who asks!
+// attributes: ['id', 'email']
